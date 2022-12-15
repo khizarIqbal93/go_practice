6f7596215c5f8e047a584c76afebc1b3d4c893fb
Khizar Iqbal <khizar.i0066@gmail.com>
2022-12-15 12:06:28 +0000

    basic set up

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type CrawlRequest struct {
+	Url   string `json:"name"`
+	Depth int    `json:"depth"`
+}
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	body, _ := io.ReadAll(r.Body)
+	data := CrawlRequest{}
+	json.Unmarshal(body, &data)
+	w.WriteHeader(http.StatusCreated)
+}
 
 func main() {
-	log.Println("Hello World")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/category", requestHandler).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
