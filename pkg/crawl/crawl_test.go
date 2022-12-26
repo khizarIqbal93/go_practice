74a97283e197494c073674618ef7218e9f9c60ef
Khizar Iqbal <khizar.i0066@gmail.com>
2022-12-26 14:40:52 +0000

    basic tests added

--- pkg/crawl/crawl_test.go
+++ pkg/crawl/crawl_test.go
@@ -3,13 +3,22 @@ package crawl
 import "testing"
 
 type urlTest struct {
-	urlString string
-	isRoot    int
-	expected  Page
+	urlString         string
+	isRoot            bool
+	expectedUrlString string
+}
+
+var urlTests = []urlTest{
+	urlTest{"https://go.dev/doc/code#Testing", true, "https://go.dev/doc/code#Testing"},
+	urlTest{"https://www.youtube.com/watch?v=IbFGG4T3_Yo", true, "https://www.youtube.com/watch?v=IbFGG4T3_Yo"},
 }
 
 func TestSetPageUrl(t *testing.T) {
-	// cases := []struct{
-
-	// }
+	for _, test := range urlTests {
+		page := Page{}
+		page.SetPageUrl(test.urlString, test.isRoot)
+		if page.Page != test.expectedUrlString {
+			t.Errorf("Output not equal to expected %q", test.expectedUrlString)
+		}
+	}
 }
